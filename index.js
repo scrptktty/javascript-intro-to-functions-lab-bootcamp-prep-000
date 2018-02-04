e04bc4f5a3852d95cf78683cf239df767556cb32
Kachel Wittig <kjwittig@gmail.com>
2018-02-04 23:38:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,10 +15,10 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string === lowercase.toLowerCase) {
+  if (string === lowercase.toLowerCase)
     return "I can't hear you"
-  } else { if (string === uppercase.toUpperCase)
+  else if (string === uppercase.toUpperCase)
     return "YES INDEED"
   else if (string === "I love you, Grandma")
     return "I love you, too."
-} }
+}
